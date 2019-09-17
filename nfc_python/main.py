66adc4149b931c5681ff33bf57856496e378f553
Stefan Schokker <stefan.schokker@icloud.com>
2019-09-17 15:59:18 +0200

    writing nfc

--- nfc_python/main.py
+++ nfc_python/main.py
@@ -10,22 +10,19 @@ rdr = RFID()
 util = rdr.util()
 # Set util debug to true - it will print what's going on
 util.debug = True
-
 run = True
 
 
-def end_read(signal,frame):
+def end_read(frame):
     global run
-    print("\nCtrl+C captured, ending read.")
+    print("\nSIGINT captured, ending read.")
     run = False
     rdr.cleanup()
     sys.exit()
 
 
+# Set sigint catcher
 signal.signal(signal.SIGINT, end_read)
-
-# valid sections white card: S1B0, S1B2
-# valid sections blue drop:
 
 while run:
     rdr.wait_for_tag()
@@ -36,18 +33,18 @@ while run:
 
     (error, uid) = rdr.anticoll()
     if not error:
-        print("Card read UID:")
-        for a in uid:
-            print(" " + str(a))
-
-        print("Setting tag")
+        print("Card read UID: "+str(uid[0])+", "+str(uid[1])+", "+str(uid[2])+", "+str(uid[3])+", "+str(uid[4])+"]")
+        print("Select given card")
         util.set_tag(uid)
         print("\nAuthorizing")
-        # util.auth(rdr.auth_a, [0x12, 0x34, 0x56, 0x78, 0x96, 0x92])
-        # util.auth(rdr.auth_b, [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]) # White card S1B0
-        # util.auth(rdr.auth_b, [0x74, 0x00, 0x52, 0x35, 0x00, 0xFF]) # Blue drop S1B0
-        print("\nReading")
-        util.read_out(4)
+        util.auth(rdr.auth_b, [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]) # White card S1B0
+        # util.auth(rdr.auth_b, [0x74, 0x00, 0x52, 0x35, 0x00, 0xFF])
+        bank = 4
+
+        print("writing: " + str(util.rewrite(bank, [250])))
+
+        util.read_out(bank)
+
         print("\nDeauthorizing")
         util.deauth()
 
